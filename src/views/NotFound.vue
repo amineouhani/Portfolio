eb2a23fc2a98578184899812ef5ad9b59e5fbe80
Amine <amineprojet7@gmail.com>
2021-01-23 02:11:45 +0100

    missing saved file

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,13 +2,19 @@
   <div class="container auto">
     <div class="big">
       <h1>
-           <span class="fancy">A</span>n <span class="fancy">E</span>rror <span class="fancy">I</span>s <span class="fancy">O</span>ccured<span class="fancy"> p<whitecol>lease</whitecol> c<whitecol>ome</whitecol> b<whitecol>ack</whitecol>.</span>
+        <span class="fancy">A</span>n <span class="fancy">E</span>rror
+        <span class="fancy">I</span>s <span class="fancy">O</span>ccured<span
+          class="fancy"
+        >
+          p<whitecol>lease</whitecol> c<whitecol>ome</whitecol>
+          b<whitecol>ack</whitecol>.</span
+        >
 
-           <font-awesome-icon
-            class="fancy"
-            style="    font-size: 40px; margin-left: 9px"
-            :icon="['fas', 'exclamation-circle']"
-          />
+        <font-awesome-icon
+          class="fancy"
+          style="font-size: 40px; margin-left: 9px"
+          :icon="['fas', 'exclamation-circle']"
+        />
       </h1>
     </div>
   </div>
@@ -41,8 +47,5 @@ span,
 div,
 p {
   animation: 0.4s cubic-bezier(0.22, 1, 0.36, 1) 0s 1 slideInFromLeft;
-}
-whitecol {
-    color: #fbfbfb
 }
 </style>
